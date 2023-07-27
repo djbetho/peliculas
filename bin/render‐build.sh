c98a44ef21ccfb0ed772aa37596bcb4fed816e90
djbetho <ru.bugueno@gmail.com>
2023-07-27 17:35:53 -0400

    Update render‐build.sh

--- bin/render‐build.sh
+++ bin/render‐build.sh
@@ -6,5 +6,10 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+bundle exec rake db:drop
+
+bundle exec rake db:create
+
 bundle exec rake db:migrate
 bundle exec rake db:seed
