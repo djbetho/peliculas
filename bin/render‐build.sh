da6e6774db47195ae93933b7fdf9238e284fe788
djbetho <ru.bugueno@gmail.com>
2023-07-27 17:37:02 -0400

    Update render‐build.sh

--- bin/render‐build.sh
+++ bin/render‐build.sh
@@ -7,7 +7,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-bundle exec rake db:drop
+bundle exec rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle exec rake db:create
 
